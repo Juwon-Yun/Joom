b0d2782dfb551e9534230a4ac09b2c512806d69d
Juwon-Yun <eooll2@daum.net>
2021-09-17 17:01:27 +0900

    9월 17일

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,18 +1,65 @@
+const messageList = document.querySelector("ul");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
 // socket -> 서버로 부터의 연결 (브라우저와 서버간의)
 const socket = new WebSocket(`ws://${window.location.host}`);
 
-socket.addEventListener("open", ()=>{
+function makeMessage(type, payload){
+    //String을 보내기전에 Object를 만들고  JSON문자열로 반환한다.
+    const msg = {type, payload};
+    return JSON.stringify(msg);
+}
+
+
+socket.addEventListener("open", () => {
     console.log("Connected to Server check!!!");
 });
 
-socket.addEventListener("message", (message)=>{
-    console.log("New Message : ", message.data);
-});
+ //socket.addEventListener("message", (message)=>{
+ //    const li = document.createElement("li");
+ //    li.innerText = message.data;
+ //    messageList.append(li);
+ //});
 
-socket.addEventListener("close", ()=>{
+socket.addEventListener('message', async (message) => {
+     if (typeof message.data === 'string') {
+        const li = document.createElement("li");
+        li.innerText = message.data;
+        messageList.append(li);
+
+        //console.log(message.data);
+     } else {
+
+         const messageText = await message.data.text();
+         const li = document.createElement("li");
+         li.innerText = messageText;
+         messageList.append(li);
+
+         //console.log(messageText);
+     }
+ });
+
+
+socket.addEventListener("close", () => {
     console.log("DisConnected to Server Close@@@");
 });
 
-setTimeout(()=>{
-    socket.send("hello from the browser!");
-},5000);
+function handleSubmit(event) {
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(makeMessage("new_message", input.value));
+    input.value = "";
+};
+
+
+function handleNickSubmit(event){
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+
+    //문자열로 보내야하니 JSON Object를 전달한다
+    socket.send(makeMessage("nickname", input.value));
+    input.value = "";
+};
+
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -18,17 +18,45 @@ const server = http.createServer(app);
 // Websocket 서버 만들기 (이렇게하면 http, ws 서버 두개를 돌릴 수 있다, 같은 포트(3000)으로 만들기 위해 둘다 선언)
 const wss = new WebSocket.Server({ server });
 
+const sockets = [];
+
 // socket -> 연결된 브라우저를 표시, connection이 생기면 socket을 받는 함수
 wss.on("connection", (socket)=>{
+    
+    sockets.push(socket);
+    socket["nickname"] = "익명";
     console.log("Connected to Browser check!!!");
+
+    // 익명 함수 () => {};
     socket.on("close", ()=>{
         console.log("DisConnected from the Browser Close@@@");
     });
-    socket.on("message", (message)=>{
-        console.log(message);
+
+    socket.on("message", (msg)=>{
+        const message = JSON.parse(msg);
+        // console.log(parsed, message, parsed.type, parsed.payload);
+
+        switch(message.type){
+            case "new_message":
+                sockets.forEach(aSocket => aSocket.send(`${socket.nickname}:${message.payload}`));
+                break;
+            case "nickname":
+                //console.log(message.payload);
+                //닉네임을 입력했을경우
+                socket["nickname"] = message.payload;
+                break;
+        }
     });
-    socket.send("hello!!!");
+
+    //socket.on('message', (message, isBinary) => {
+    //    const messageString = isBinary ? message : message.toString('utf8');
+    //    socket.send(messageString);
+    //});
+
+    //socket.send("hello!!!");
 });
 
 // http서버 위에 ws 서버를 만듬 (3000번 포트에서 http, ws 두곳의 request를 처리할 수 있다)
 server.listen(3000, handleListen);
+
+
